55971cf93612d1769d0dd8b7a167d8ad181afd5e
leonard2108 <barrycross29@gmail.com>
2025-12-03 10:11:21 -0300

    Update get-bg.js

--- api/get-bg.js
+++ api/get-bg.js
@@ -1,1 +1,20 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
 
+  const fs = require("fs");
+  const path = require("path");
+
+  const file = path.join(process.cwd(), "public-bg.json");
+
+  if (!fs.existsSync(file)) {
+    return res.status(200).json({
+      bgUrl: "",
+      bgFit: "cover",
+      bgPos: "center center",
+      bgOpacity: "0.15"
+    });
+  }
+
+  const data = JSON.parse(fs.readFileSync(file, "utf8"));
+  res.status(200).json(data);
+}
